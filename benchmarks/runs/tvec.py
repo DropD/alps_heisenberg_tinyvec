92a4b7e28709f81be2c7a52bbe6cf24a912bc316
DropD <r.haeuselmann@gmx.ch>
2014-09-08 13:54:58 +0200

    add ultimate experiment.

---
+++ benchmarks/runs/tvec.py
@@ -0,0 +1,95 @@
+import matplotlib.pyplot as plt
+from ethpy import datio as dio
+from ethpy import fncplot as fpl
+from matplotlib import rc
+#~ rc('text', usetex=True)
+import sys
+import subprocess32 as sp
+
+runs = ['debug_uv_native', 'rwd_uv_native', 'r_uv_native',
+        'debug_tv_native', 'rwd_tv_native', 'r_tv_native'
+       ]
+result_files = [i+'.out.dat' for i in runs]
+
+if 'make' in sys.argv:
+    bdir = ['../build-d/', '../build-rwd/', '../build-r/']
+    for i in range(len(bdir)):
+        sp.check_call(['make', '-C', bdir[i], 'rebuild_cache'])
+        sp.check_call(['make', '-C', bdir[i], 'pb_ur_native', 'pb_tv_native'])
+    print 'build phase ended'
+
+if 'run' in sys.argv:
+    from os import path
+    for i in range(len(runs)):
+        r = runs[i]
+        print 'running ' + r
+
+        if 'debug_' in r: pdir = '../build-d/'
+        elif 'rwd_' in r: pdir = '../build-rwd/'
+        elif 'r_'   in r: pdir = '../build-r/'
+
+        if '_uv_' in r: popt = 'uv'
+        elif '_tv_' in r: popt = 'tv'
+
+        if 'i386' in r: pname = 'pb_{}_i386'.format(popt)
+        elif 'native' in r: pname = 'pb_{}_native'.format(popt)
+
+        with open(result_files[i], 'w') as resf:
+            sp.check_call([path.join(pdir, pname)], stdout = resf)
+    print 'run phase ended'
+
+results = {runs[i] : dio.read(result_files[i]) for i in range(len(runs))}
+
+x = results[runs[0]][:,0]
+
+#----------------------- hand vectorized vs template vectorized -------------------------------
+
+yh = results['r_uv_native'][:,1:]
+yt = results['r_tv_native'][:,1:]
+
+plt.figure()
+plt.plot(x, yh[:, 0], label =  '2D hand', color = 'b', ls = '-')
+plt.plot(x, yh[:, 1], label =  '4D hand', color = 'g', ls = '-')
+plt.plot(x, yh[:, 2], label =  '8D hand', color = 'r', ls = '-')
+plt.plot(x, yh[:, 3], label = '16D hand', color = 'c', ls = '-')
+plt.plot(x, yh[:, 4], label = '32D hand', color = 'm', ls = '-')
+
+plt.plot(x, yt[:, 0], label =  '2D template', color = 'b', ls = '--')
+plt.plot(x, yt[:, 1], label =  '4D template', color = 'g', ls = '--')
+plt.plot(x, yt[:, 2], label =  '8D template', color = 'r', ls = '--')
+plt.plot(x, yt[:, 3], label = '16D template', color = 'c', ls = '--')
+plt.plot(x, yt[:, 4], label = '32D template', color = 'm', ls = '--')
+
+plt.xscale('log')
+plt.yscale('log')
+plt.legend(loc = 'best')
+fpl.xlabel('size')
+fpl.ylabel('# cycles')
+fpl.title(r'hand unrolled vectorized vs template unrolled vectorized \\ Release flags')
+plt.savefig('hvt_runtime.pdf')
+
+#----------------------- speedup per size and dim -------------------------------
+s = results['r_uv_native'] / results['r_tv_native']
+
+plt.figure()
+plt.plot(x, s[:,1], label = '2D')
+plt.plot(x, s[:,2], label = '4D')
+plt.plot(x, s[:,3], label = '8D')
+plt.plot(x, s[:,4], label = '16D')
+plt.plot(x, s[:,5], label = '32D')
+plt.hlines(1.0, 1e1, 1e5, colors = 'k', linestyles = '--')
+
+plt.xscale('log')
+plt.legend(loc = 'best')
+fpl.xlabel('size')
+fpl.ylabel('speedup')
+fpl.title(r'speedup template vs hand (should be 1)')
+plt.savefig('hvt_speedup.pdf')
+ 
+
+
+
+
+
+
+
