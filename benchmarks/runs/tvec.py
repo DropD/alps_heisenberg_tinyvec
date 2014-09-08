43c29c295959ed8345a8e6bfc375e2d749d94412
Rico Häuselmann <r.haeuselmann@gmx.ch>
2014-09-08 14:03:47 +0200

    fix ultimate experiment (tvec.py)

--- benchmarks/runs/tvec.py
+++ benchmarks/runs/tvec.py
@@ -7,7 +7,8 @@ import sys
 import subprocess32 as sp
 
 runs = ['debug_uv_native', 'rwd_uv_native', 'r_uv_native',
-        'debug_tv_native', 'rwd_tv_native', 'r_tv_native'
+        'debug_tv_native', 'rwd_tv_native', 'r_tv_native',
+        'debug_no_native', 'rwd_no_native', 'r_no_native'
        ]
 result_files = [i+'.out.dat' for i in runs]
 
@@ -15,7 +16,7 @@ if 'make' in sys.argv:
     bdir = ['../build-d/', '../build-rwd/', '../build-r/']
     for i in range(len(bdir)):
         sp.check_call(['make', '-C', bdir[i], 'rebuild_cache'])
-        sp.check_call(['make', '-C', bdir[i], 'pb_ur_native', 'pb_tv_native'])
+        sp.check_call(['make', '-C', bdir[i], 'pb_uv_native', 'pb_tv_native'])
     print 'build phase ended'
 
 if 'run' in sys.argv:
@@ -42,10 +43,35 @@ results = {runs[i] : dio.read(result_files[i]) for i in range(len(runs))}
 
 x = results[runs[0]][:,0]
 
+#----------------------- simple vs template vectorized -------------------------------
+
+yn = results['r_no_native'][:,1:]
+yt = results['r_tv_native'][:,1:]
+
+plt.figure()
+plt.plot(x, yn[:, 0], label =  '2D hand', color = 'b', ls = '-')
+plt.plot(x, yn[:, 1], label =  '4D hand', color = 'g', ls = '-')
+plt.plot(x, yn[:, 2], label =  '8D hand', color = 'r', ls = '-')
+plt.plot(x, yn[:, 3], label = '16D hand', color = 'c', ls = '-')
+plt.plot(x, yn[:, 4], label = '32D hand', color = 'm', ls = '-')
+
+plt.plot(x, yt[:, 0], label =  '2D template', color = 'b', ls = '--')
+plt.plot(x, yt[:, 1], label =  '4D template', color = 'g', ls = '--')
+plt.plot(x, yt[:, 2], label =  '8D template', color = 'r', ls = '--')
+plt.plot(x, yt[:, 3], label = '16D template', color = 'c', ls = '--')
+plt.plot(x, yt[:, 4], label = '32D template', color = 'm', ls = '--')
+
+plt.xscale('log')
+plt.yscale('log')
+plt.legend(loc = 'best')
+fpl.xlabel('size')
+fpl.ylabel('# cycles')
+fpl.title(r'simple vs template unrolled vectorized \\ Release flags')
+plt.savefig('svt_runtime.pdf')
+ 
 #----------------------- hand vectorized vs template vectorized -------------------------------
 
 yh = results['r_uv_native'][:,1:]
-yt = results['r_tv_native'][:,1:]
 
 plt.figure()
 plt.plot(x, yh[:, 0], label =  '2D hand', color = 'b', ls = '-')
@@ -67,6 +93,25 @@ fpl.xlabel('size')
 fpl.ylabel('# cycles')
 fpl.title(r'hand unrolled vectorized vs template unrolled vectorized \\ Release flags')
 plt.savefig('hvt_runtime.pdf')
+
+#----------------------- speedup simple v tv per size and dim -------------------------------
+s = results['r_no_native'] / results['r_tv_native']
+
+plt.figure()
+plt.plot(x, s[:,1], label = '2D')
+plt.plot(x, s[:,2], label = '4D')
+plt.plot(x, s[:,3], label = '8D')
+plt.plot(x, s[:,4], label = '16D')
+plt.plot(x, s[:,5], label = '32D')
+plt.hlines(1.0, 1e1, 1e5, colors = 'k', linestyles = '--')
+
+plt.xscale('log')
+plt.legend(loc = 'best')
+fpl.xlabel('size')
+fpl.ylabel('speedup')
+fpl.title(r'speedup template vec vs simple')
+plt.savefig('hvt_speedup.pdf')
+ 
 
 #----------------------- speedup per size and dim -------------------------------
 s = results['r_uv_native'] / results['r_tv_native']
