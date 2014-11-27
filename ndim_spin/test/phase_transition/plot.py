a04f7eaaf3b859e99896c37645a06ac83f03391a
Rico Häuselmann <r.haeuselmann@gmx.ch>
2014-11-27 19:36:28 +0100

    [wip] prepare final example experiment.

--- ndim_spin/test/phase_transition/plot.py
+++ ndim_spin/test/phase_transition/plot.py
@@ -1,82 +1,99 @@
-import subprocess32, sys
-sys.path.append('/opt/alps/lib')
-import pyalps
+import subprocess32 as sp, 
+import sys, os, argparse, pyalps, re
 from matplotlib import pyplot as plt
 import numpy as np
-#~ sys.path.append('/Users/ricoh/Prog/python/mdebug/')
-#~ from mdebug import mdebug
-#~ from analyzer import name_analyzer
-#~ DBG = mdebug(analyzer = name_analyzer())
 
 def h5get(ar, meas):
     base_path = 'simulation/results/{}/mean'.format(meas)
     val = base_path + '/value'
     err = base_path + '/error'
-    return np.array((ar[val], ar[err]))
+    return np.array([ar[val], ar[err]], dtype=np.dtype([('value', np.float), ('error', np.float)]))
 
-NUM_RUNS = 13
-parameter_files = ['param.txt.task{}.in.xml'.format(i) for i in range(1, NUM_RUNS + 1)]
-result_files    = ['param.txt.task{}.in.out.h5'.format(i) for i in range(1, NUM_RUNS + 1)]
-h5_T_path       = 'parameters/T'
-h5_L_path       = 'parameters/L'
-program         = '../../build-release/single'
-plot_file       = 'mag_sus.pdf'
-corr_file       = 'corr.pdf'
+parser = argparse.ArgumentParser(description='Run and analyze heisenberg simulation experiment')
+subparsers = parser.add_subparsers()
 
-if __name__ == '__main__':
-    if 'run' in sys.argv:
-        subprocess32.call(['/opt/alps/bin/parameter2xml', 'param.txt'])
-        for run in range(NUM_RUNS):
-            run_out = subprocess32.check_output([program, parameter_files[run]])
+sp_run = subparsers.add_parser('run')
+sp_analyze = subparsers.add_parser('anayze')
+
+parser.add_argument('infile', default='param.txt')
+
+sp_run.add_argument('-j')
+sp_run.add_argument('--no-analysis', action='store_true')
+sp_run.add_argument('program')
+sp_run.set_defaults(func=run_exp)
+
+sp_replot.set_defaults(func=plot)
+
+def run_exp(args):
+    sp.call(['parameter2xml', args.infile])
+    runs = [f for f in os.listdir(os.getcwd()) if re.match('{}.task\d*.in.xml'.format(args.infile), f)]
+    if args.j:
+        prog_call = ['parallel', '-j', args.j, args.program, ':::']
+        prog_call.extend(runs)
+        run_out = sp.check_output(prog_call)
+    else:
+        for run_infile in runs:
+            run_out = sp.check_output([args.program, run_infile])
             print '--- run {} ---'.format(run)
             print run_out
+    if not args.no_analysis:
+        analyze(args)
 
-    if 'plot' in sys.argv:
-        y_err = []
-        y = []
-        x = []
+def get_chi(result_files)
+    ar  = pyalps.hdf5.iArchive(run_outfile)
+    T   = np.array([p['T'] for p in pyalps.loadProperties(result_files)])
+    V   = np.array([p['L']**3 for p in pyalps.loadProperties(result_files)])
+    mm  = np.array([mag[0].y[0].sq() for mag in pyalps.loadMeasurements(result_files, what = 'Magnetization')])
+    m2  = np.array([msq[0].y[0] for mag in pyalps.loadMeasurements(result_files, what = 'magnetization^2')])
 
-        for run in range(NUM_RUNS):
-            ar  = pyalps.hdf5.iArchive(result_files[run])
-            T   = float(ar[h5_T_path])
-            L   = float(ar[h5_L_path])
-            m   = h5get(ar, 'Magnetization')
-            m2  = h5get(ar, 'Magnetization^2')
+    beta = 1. / T
+    chi = beta * V * (m2 - mm)
 
-            beta = 1. / T
-            V = L * L * L
-            mm = [np.dot(m[i,:], m[i,:]) for i in [0,1]]
-            chi = beta * V * (m2 - mm)
+    return np.array(zip(T, chi), dtype = [('T', np.float), ('chi', np.float)])
 
-            y.append(chi[0])
-            y_err.append(chi[1])
-            x.append(T)
+def get_corr(result_file):
+    ar  = pyalps.hdf5.iArchive(run_outfile)
+    corr = h5get(ar, 'Correlations')
+    dist = h5get(ar, 'Distances')
 
-        x = np.array(x)
-        y = np.array(y)
-        y_err = np.array(y_err)
+    corr_data = np.array(zip(dist, corr), 
+                         dtype = [('dist', np.float), ('corr', np.float)])
+    corr_data.sort(order='dist')
+    
+    g = np.array([(dist, 0., 0.) for dist in np.unique(corr_data['dist'])],
+                 dtype = [('dist', np.float), ('corr', np.float), ('err', np.float)])
 
-        plt.figure()
-        plt.fill_between(x, y+y_err, y-y_err)
-        plt.xlabel(r'$\frac{T}{K}$')
-        plt.ylabel(r'$\chi$')
-        plt.savefig(plot_file)
-        subprocess32.call(['open', plot_file])
+def analyze(args)
+    runs = pyalps.getResultFiles(prefix = args.infile)
 
-        plt.figure()
-        corr = h5get(ar, 'Correlations')
-        dist = h5get(ar, 'Distances')
-        xc = []
-        yc = []
-        yc_e = []
-        for d in dist[0]:
-            if not d in xc:
-                xc.append(d)
-                cd = [corr[0][i] for i in range(len(corr[0])) if dist[0][i] == d]
-                yc.append(np.mean(cd))
-                yc_e.append(np.std(cd))
-        c = np.array([(xc[i], abs(yc[i]), yc_e[i]) for i in range(len(xc))], dtype = [('dist', float), ('corr', float), ('err', float)])
-        c.sort(order='dist')
-        plt.fill_between(c['dist'], c['corr'] + c['err'], c['corr'] - c['err'])
-        plt.savefig(corr_file)
-        subprocess32.call(['open', corr_file])
+    chi_data = get_chi()
+    T = chi_data['T']
+    chi = [c.mean for c in chi_data['chi']]
+    chi_err = [c.error for c in chi_data['chi']]
+
+    plt.figure()
+    plt.errorbar(T, chi, yerr = chi_err)
+    plt.vline(1. / 0.693035)
+    plt.xlabel(r'Temperature $\frac{T}{J}$')
+    plt.ylabel(r'Magnetic Susceptibility $\chi$')
+    plt.savefig('{}.plot_chi.pdf'.format(args.infile))
+
+    plt.figure()
+    corr = h5get(ar, 'Correlations')
+    dist = h5get(ar, 'Distances')
+    xc = []
+    yc = []
+    yc_e = []
+    for d in dist[0]:
+        if not d in xc:
+            xc.append(d)
+            cd = [corr[0][i] for i in range(len(corr[0])) if dist[0][i] == d]
+            yc.append(np.mean(cd))
+            yc_e.append(np.std(cd))
+    c = np.array([(xc[i], abs(yc[i]), yc_e[i]) for i in range(len(xc))], dtype = [('dist', float), ('corr', float), ('err', float)])
+    c.sort(order='dist')
+    plt.errorbar(c['dist'], c['corr'], yerr = c['err'])
+    plt.savefig(corr_file)
+
+if __name__ == '__main__':
+    parser.parse_args()
